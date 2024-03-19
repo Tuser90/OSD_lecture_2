383f86f03e49a5d36f7f210ff0c381dd772efd91
Md Tuser Emran <tuseremran90@gmail.com>
2024-03-19 13:11:00 +0100

    Vehicle

--- Vehicle.py
+++ Vehicle.py
@@ -29,19 +29,42 @@ class Vehicle:
     def get_fuel_type(self):
         return self.fuel_type
 
-# Create a new vehicle object
-car = Vehicle("Honda Accord", "Accord123", "Blue", "Gasoline")
 
-# Get vehicle attributes
-print("Car Name:", car.get_name())
-print("License Plate:", car.get_license_plate())
-print("Color:", car.get_color())
-print("Fuel Type:", car.get_fuel_type())
+class Car(Vehicle):
+    def __init__(self, name, license_plate, color, fuel_type, num_doors):
+        super().__init__(name, license_plate, color, fuel_type)
+        self.num_doors = num_doors
 
-# Update vehicle attributes
-car.set_color("Red")
-car.set_fuel_type("Diesel")
+    def set_num_doors(self, num_doors):
+        self.num_doors = num_doors
 
-# Get updated vehicle attributes
-print("\nUpdated Color:", car.get_color())
-print("Updated Fuel Type:", car.get_fuel_type())
+    def get_num_doors(self):
+        return self.num_doors
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, name, license_plate, color, fuel_type, num_wheels):
+        super().__init__(name, license_plate, color, fuel_type)
+        self.num_wheels = num_wheels
+
+    def set_num_wheels(self, num_wheels):
+        self.num_wheels = num_wheels
+
+    def get_num_wheels(self):
+        return self.num_wheels
+
+
+# Usage example:
+car = Car("Honda Accord", "Emran123", "Blue", "Gasoline", 4)
+print("Car Name:", car.get_name())g
+print("Car License Plate:", car.get_license_plate())
+print("Car Color:", car.get_color())
+print("Car Fuel Type:", car.get_fuel_type())
+print("Number of Doors:", car.get_num_doors())
+
+motorcycle = Motorcycle("Honda", "XYZ456", "Black", "Gasoline", 2)
+print("\nMotorcycle Name:", motorcycle.get_name())
+print("Motorcycle License Plate:", motorcycle.get_license_plate())
+print("Motorcycle Color:", motorcycle.get_color())
+print("Motorcycle Fuel Type:", motorcycle.get_fuel_type())
+print("Number of Wheels:", motorcycle.get_num_wheels())
